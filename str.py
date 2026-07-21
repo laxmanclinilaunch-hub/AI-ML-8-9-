0d4420988b8fa6f05d699e4c931e185ec1269ae1
laxman <laxman.clinilaunch@gmail.com>
2026-07-21 20:49:24 +0530

    added_notes_upto_if_elif_else

---
+++ str.py
@@ -0,0 +1,15 @@
+a=[1,2,3,98,7,295,"pratik","divya",1,2,3,98,7,295,"pratik","divya"]
+
+print(f"this is my list :{a}")
+
+print()
+
+b=(1,2,3,98,7,295,"pratik","divya",1,2,3,98,7,295,"pratik","divya")
+
+print(f"this is my tuples {b}")
+
+print()
+c={98,1,2,3,7,295,"pratik","divya",1,2,3,98,7,295,"pratik","divya"}
+
+
+print(f'this is my set {c}')
